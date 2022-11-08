cc577af582f6f5d14dd02dbadab6cf9bf77a0d60
arielbg00 <ariel-bravo96@hotmail.com>
2022-11-08 12:21:02 -0300

    creacion contenedor de filtros

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -4,20 +4,30 @@ import { useDispatch } from "react-redux";
 export default function Filters() {
 
    const dispatch = useDispatch();
-
-   const handleFilters = (e) => {
-      // dispatch();
-   };
    
    return (
       <div>
-         <select onChange={(e) => handleFilters(e)}>
-            <option>Ordenar por</option>
-            <option value="categoria">Categoría</option>
-            <option value="transmision">Transmisión</option>
-            <option value="caracteristicas">Características</option>
-            <option value="marca">Marca</option>
-         </select>
+         <div>
+            <label>Categoría: </label>
+            <input type="checkbox" name="chico" />chico
+            <input type="checkbox" name="mediano" />mediano
+            <input type="checkbox" name="grande" />grande
+            <input type="checkbox" name="premiun" />premiun
+            <input type="checkbox" name="convertible" />convertible
+         </div>
+         <div>
+            <label>Transmisión: </label>
+            <input type="checkbox" name="manual" />manual
+            <input type="checkbox" name="automatico" />automatico
+         </div>
+         <div>
+            <label>Características: </label>
+            <input type="checkbox" name="manual" />Aire Acondicionado
+            <input type="checkbox" name="automatico" />+4 asientos
+         </div>
+         <div>
+            <label>Marca: </label>
+         </div>
       </div>
    );
 }
