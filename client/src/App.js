11debc4dacd429c039efa312782f5115f1999e46
Javymoyano <javymoyano80@gmail.com>
2022-11-08 14:32:05 -0300

    Esta hecha la ruta del Home en App. Solo para probar rederizado

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import Home from "./components/Home/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/home" component={Home} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
