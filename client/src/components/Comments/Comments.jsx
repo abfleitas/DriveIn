9d89774fc939e88cdc1a94a7b884fb8b2c7b33aa
luqasmagra <luqasmagra@gmail.com>
2022-11-09 20:28:07 -0300

    h1 fixed

--- client/src/components/Comments/Comments.jsx
+++ client/src/components/Comments/Comments.jsx
@@ -25,7 +25,7 @@ export default function Comments() {
     <>
       <div className="ml-[20%] flex items-center text-2xl font-semibold">
         <i className="fa-solid fa-star mr-5"></i>
-        <h1>DESTINOS FAVORITOS</h1>
+        <h1>COMENTARIOS DESTACADOS</h1>
       </div>
       <div class="sm:flex flex-wrap justify-center items-center text-center gap-8 pb-5">
         <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4 bg-[#F97D67] mt-6  shadow-lg rounded-lg ">
