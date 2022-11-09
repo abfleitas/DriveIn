bcc91077428aa6c4bf24cb1104207313dc42f534
Javier Villatoro <wemakesoftx@gmail.com>
2022-11-09 15:24:06 -0600

    feat(client/redux): Agregar autos a favoritos, localStorage pendiente.

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,3 +1,4 @@
+
 export const FILTER_CATEGORY = "FILTER_CATEGORY";
 export const FILTER_TRANSMISSION = "FILTER_TRANSMISSION";
 export const FILTER_CHARACTERISTICS = "FILTER_CHARACTERISTICS";
@@ -5,44 +6,43 @@ export const FILTER_BRAND = "FILTER_BRAND";
 export const FILTER_PRICE = "FILTER_PRICE";
 export const ADD_FAVORITES = "ADD_FAVORITES";
 
-
 export const filterCategory = (payload) => {
-   return {
-      type: "FILTER_CATEGORY",
-      payload
-   };
+  return {
+    type: "FILTER_CATEGORY",
+    payload,
+  };
 };
 
 export const filterTransmission = (payload) => {
-   return {
-      type: "FILTER_TRANSMISSION",
-      payload
-   };
+  return {
+    type: "FILTER_TRANSMISSION",
+    payload,
+  };
 };
 
 export const filterCharacteristics = (payload) => {
-   return {
-      type: "FILTER_CHARACTERISTICS",
-      payload
-   };
+  return {
+    type: "FILTER_CHARACTERISTICS",
+    payload,
+  };
 };
 
 export const filterBrand = (payload) => {
-   return {
-      type: "FILTER_BRAND",
-      payload
-   };
+  return {
+    type: "FILTER_BRAND",
+    payload,
+  };
 };
 export const filterPrice = (payload) => {
-   return {
-      type: "FILTER_PRICE",
-      payload
-   };
+  return {
+    type: "FILTER_PRICE",
+    payload,
+  };
 };
 //Action agregar auto a favoritos.
 export const addFavorites = (auto) => {
    return {
-      type: "ADD_FAVORITES",
+      type: 'ADD_FAVORITES',
       payload: auto
    }
-}
+};
