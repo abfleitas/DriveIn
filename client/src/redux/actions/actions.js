15d49b7eb62f4147fc353491aa566c505982d3a7
luqasmagra <luqasmagra@gmail.com>
2022-11-10 17:49:35 -0300

    Search y Carrusel, conectados con back

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,52 +1,95 @@
-
+import axios from "axios";
 export const FILTER_CATEGORY = "FILTER_CATEGORY";
 export const FILTER_TRANSMISSION = "FILTER_TRANSMISSION";
 export const FILTER_CHARACTERISTICS = "FILTER_CHARACTERISTICS";
 export const FILTER_BRAND = "FILTER_BRAND";
 export const FILTER_PRICE = "FILTER_PRICE";
 export const ADD_FAVORITES = "ADD_FAVORITES";
+export const GET_DETAILS = "GET_DETAILS";
+export const GET_COUNTRIES = "GET_COUNTRIES";
+export const GET_CITIES = "GET_CITIES";
+export const GET_CITIES_FEATURED = "GET_CITIES_FEATURED";
 
 export const filterCategory = (payload) => {
   return {
-    type: "FILTER_CATEGORY",
+    type: FILTER_CATEGORY,
     payload,
   };
 };
 
 export const filterTransmission = (payload) => {
   return {
-    type: "FILTER_TRANSMISSION",
+    type: FILTER_TRANSMISSION,
     payload,
   };
 };
 
 export const filterCharacteristics = (payload) => {
   return {
-    type: "FILTER_CHARACTERISTICS",
+    type: FILTER_CHARACTERISTICS,
     payload,
   };
 };
 
 export const filterBrand = (payload) => {
   return {
-    type: "FILTER_BRAND",
+    type: FILTER_BRAND,
     payload,
   };
 };
 export const filterPrice = (payload) => {
   return {
-    type: "FILTER_PRICE",
+    type: FILTER_PRICE,
     payload,
   };
 };
 //Action agregar auto a favoritos.
 export const addFavorites = (auto) => {
-   return {
-      type: 'ADD_FAVORITES',
-      payload: auto
-   }
+  return {
+    type: ADD_FAVORITES,
+    payload: auto,
+  };
 };
 
 export const setVehicleDetailsState = (data) => {
-  return { type: "SETVEHICLEDETAILSSTATE", payload: data};
-}
+  return { type: GET_DETAILS, payload: data };
+};
+
+export const getCountries = () => {
+  return function (dispatch) {
+    axios.get("http://localhost:3001/country").then(
+      (response) => {
+        dispatch({ type: GET_COUNTRIES, payload: response.data });
+      },
+      (error) => {
+        return error;
+      }
+    );
+  };
+};
+
+export const getCities = (country) => {
+  return function (dispatch) {
+    axios.get(`http://localhost:3001/cities?country=${country}`).then(
+      (response) => {
+        dispatch({ type: GET_CITIES, payload: response.data });
+      },
+      (error) => {
+        return error;
+      }
+    );
+  };
+};
+
+export const getCitiesFeatured = () => {
+  return function (dispatch) {
+    axios.get("http://localhost:3001/cities/destacadas").then(
+      (response) => {
+        dispatch({ type: GET_CITIES_FEATURED, payload: response.data });
+      },
+      (error) => {
+        return error;
+      }
+    );
+  };
+};
