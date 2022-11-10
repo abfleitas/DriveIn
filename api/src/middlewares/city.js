57c71ca3c816950dc7bfa497f3d710d05ebc135f
unknown <verojuy.gonzalez@gmail.com>
2022-11-10 11:30:53 -0300

    ruta city

--- api/src/middlewares/city.js
+++ api/src/middlewares/city.js
@@ -5,11 +5,24 @@ const getAllCity= async()=>{
 
      try {
         const allCities = await City.findAll()
+    
+
         return allCities
 
      } catch (error) {
         console.log()        
      }
+}
+const getcityDestacadas=async()=>{
+    try {
+        const destacadas = await City.findAll({
+            wheres:{
+                destacado: "true"
+            }
+        })
+    } catch (error) {
+        
+    }
 }
 
 
--- api/src/routes/route-city.js
+++ api/src/routes/route-city.js
@@ -1,28 +1,31 @@
 const {Router} = require('express');
 const {getAllCity} = require('../middlewares/city');
 const {City} = require('../db');
+const vehicles = require('./route-vehicles');
 const router = Router();
+const {Vehicles}=require("../db");
 
 
 router.get ("/", async(req,res)=>{
     const  name= req.query.name
     try {     
     // if(name){
+     
     //        const buscoName= await City.findAll({
     //         where:{
     //           name:{
     //             [Op.iLike]: "%" + name +"%"
     //           },
-    //         //   include:{model:vehicle}
+    // //         //   include:{model:vehicle}
     //         }
     //       })
+         
     //      return res.json(buscoName)          
-        
-    // }
-    const allCities= await getAllCity()
-    return  res.status(200).send(allCities)
-
-
+       
+    //}
+    
+    const dataCity= await getAllCity()   
+    return  res.status(200).send(dataCity)
 }catch (error) {
     res.status(404).send("la busqueda fallo")
   }
@@ -32,11 +35,22 @@ router.get("/:id", async(req, res)=>{
     const id = req.params.id.toUpperCase()
     try{
     if(!id) res.send({ msg:"id no existe"})
+        const city =await City.findOne({
+            where:{
+                id:id
+            },
+            // includes:{
+            //     model:Vehicles,
+            //     attibute:["name"]
+            // }
+            
+        })
+        res.send(city)
    
-        const city = await City.findByPk(id)
+        // const city = await City.findByPk(id,{includes:{model:Vehicles}})
         
-        console.log()
-        res.send (city)
+        // console.log()
+        // res.send (city)
     
     }catch(error){
         res.status(404).send("la busqueda fallo")
