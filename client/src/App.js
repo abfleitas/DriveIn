09ec602bea7fdbc09ede3890e4fa78e6a28d2759
Javymoyano <javymoyano80@gmail.com>
2022-11-10 08:37:21 -0300

    Pagina de Inicio/Landing que conecta con Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Inicio from "./components/Inicio/Inicio";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
+          <Route path="/" element={<Inicio />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </div>
