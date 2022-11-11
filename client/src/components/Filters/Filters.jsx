eadcfeeffb35de0734447caf1d4e7570dfeb4c1c
arielbg00 <ariel-bravo96@hotmail.com>
2022-11-11 13:58:17 -0300

    feat/fitrosCombinados

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,26 +1,15 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { filterCategory, filterTransmission, filterCharacteristics, filterBrand, filterPrice } from "../../redux/actions/actions";
+import { filterPrice, filter } from "../../redux/actions/actions";
 
 export default function Filters() {
 
    const dispatch = useDispatch();
 
-   const handleCategory = (e) => {
-      dispatch(filterCategory(e.target.value));
-   };
-
-   const handleTransmission = (e) => {
-      dispatch(filterTransmission(e.target.value));
-   };
-
-   const handleCharacteristics = (e) => {
-      dispatch(filterCharacteristics(e.target.value));
-   };
-
-   const handleBrand = (e) => {
-      dispatch(filterBrand(e.target.value));
-   };
+   const handleFilters = (e) => {
+      if (e.target.checked) dispatch(filter({key: e.target.name, value: e.target.value}));
+      if (!e.target.checked) dispatch(filter({key: e.target.name, value: "all"}));
+   }
 
    const handlePrice = (e) => {
       dispatch(filterPrice(e.target.value));
@@ -33,35 +22,35 @@ export default function Filters() {
       <div>
          <div>
             <label>Categoría: </label>
-            <input type="checkbox" id="small" value="small" onChange={handleCategory} />
+            <input type="checkbox" name="category" id="small" value="small" onChange={handleFilters} />
             <label htmlFor="small">Chico </label>
 
-            <input type="checkbox" id="medium" value="medium" onChange={handleCategory} />
+            <input type="checkbox" name="category" id="medium" value="medium" onChange={handleFilters} />
             <label htmlFor="medium">Mediano </label>
 
-            <input type="checkbox" id="big" value="big" onChange={handleCategory} />
+            <input type="checkbox" name="category" id="big" value="big" onChange={handleFilters} />
             <label htmlFor="big">Grande </label>
 
-            <input type="checkbox" id="premium" value="premium" onChange={handleCategory} />
+            <input type="checkbox" name="category" id="premium" value="premium" onChange={handleFilters} />
             <label htmlFor="premium">Premium </label>
 
-            <input type="checkbox" id="convertible" value="convertible" onChange={handleCategory} />
+            <input type="checkbox" name="category" id="convertible" value="convertible" onChange={handleFilters} />
             <label htmlFor="convertible">Convertible</label>
          </div>
          <div>
             <label>Transmisión: </label>
-            <input type="checkbox" id="manual" value="manual" onChange={handleTransmission} />
+            <input type="checkbox" name="transmition" id="manual" value="manual" onChange={handleFilters} />
             <label htmlFor="manual">Manual </label>
 
-            <input type="checkbox" id="automatic" value="automatic" onChange={handleTransmission} />
+            <input type="checkbox" name="transmition" id="automatic" value="automatic" onChange={handleFilters} />
             <label htmlFor="automatic">Automático</label>
          </div>
          <div>
             <label>Características: </label>
-            <input type="checkbox" id="AC" value="AC" onChange={handleCharacteristics} />
+            <input type="checkbox" name="AC" id="AC" value="AC" onChange={handleFilters} />
             <label htmlFor="AC">Aire Acondicionado </label>
 
-            <input type="checkbox" id="seats" value="seats" onChange={handleCharacteristics} />
+            <input type="checkbox" name="seats" id="seats" value="seats" onChange={handleFilters} />
             <label htmlFor="seats">+4 Asientos</label>
          </div>
          <div>
@@ -69,7 +58,7 @@ export default function Filters() {
             {
                brand && brand.map((el, i) => (
                   <span key={i}>
-                     <input type="checkbox" id={el} value={el} onChange={handleBrand} />
+                     <input type="checkbox" name="brand" id={el} value={el} onChange={handleFilters} />
                      <label htmlFor={el}>{el} </label>
                   </span>
                ))
--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,38 +1,15 @@
 
-export const FILTER_CATEGORY = "FILTER_CATEGORY";
-export const FILTER_TRANSMISSION = "FILTER_TRANSMISSION";
-export const FILTER_CHARACTERISTICS = "FILTER_CHARACTERISTICS";
-export const FILTER_BRAND = "FILTER_BRAND";
+export const FILTER = "FILTER";
 export const FILTER_PRICE = "FILTER_PRICE";
 export const ADD_FAVORITES = "ADD_FAVORITES";
 
-export const filterCategory = (payload) => {
+export const filter = (payload) => {
   return {
-    type: "FILTER_CATEGORY",
-    payload,
-  };
-};
+    type: "FILTER",
+    payload
+  }
+}
 
-export const filterTransmission = (payload) => {
-  return {
-    type: "FILTER_TRANSMISSION",
-    payload,
-  };
-};
-
-export const filterCharacteristics = (payload) => {
-  return {
-    type: "FILTER_CHARACTERISTICS",
-    payload,
-  };
-};
-
-export const filterBrand = (payload) => {
-  return {
-    type: "FILTER_BRAND",
-    payload,
-  };
-};
 export const filterPrice = (payload) => {
   return {
     type: "FILTER_PRICE",
