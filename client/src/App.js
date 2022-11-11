07a9f41a163a0de6c5cc95b9c1fbc456478732a5
abfleitas <leonardo.fleitas.sk@gmail.com>
2022-11-11 15:10:19 -0300

    componente de registro a terminar

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home/Home";
 import Inicio from "./components/Inicio/Inicio";
 
 import VehicleDetails from "./components/Details/Details";
+import { Register } from "./components/Register/Register";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Inicio />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register/ >}/>
           <Route path="/details/:id" element={<VehicleDetails />} />
         </Routes>
       </div>
