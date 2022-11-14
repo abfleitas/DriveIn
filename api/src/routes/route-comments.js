8173c57427525dd8953f5f30b9dd843526aaa37f
unknown <verojuy.gonzalez@gmail.com>
2022-11-14 11:28:47 -0300

    ruta comentarios

--- api/src/routes/route-comments.js
+++ api/src/routes/route-comments.js
@@ -1,17 +1,17 @@
 const { Router } = require("express");
 const router = Router();
 const { Comments } = require("../db.js");
-const {getAllComentarios} = require("../middlewares/comments");
+const {getAllComentarios } = require("../middlewares/comments");
 const comments = require("../middlewares/comments.json");
 
 router.post("/", async (req, res) => {
   try {
-    const {comments} = req.body;
-    if (!comments) {
+    const { comment } = req.body;
+    if (!comment) {
       return res.status(400).json({ error: "There is nothing to comment" });
     }
-    await Comments.create(comments);
-    return res.status(200).json(comments);
+    await Comments.create(comment);
+    return res.status(200).json(comment);
   } catch (error) {
 
     res.status(400).json(error);
@@ -20,11 +20,11 @@ router.post("/", async (req, res) => {
 });
 router.get("/", async (req, res) => {
   try {
-         
-    // const comments = await Comments.findAll({
+
+    // const comentarios = await Comments.findAll({
     //   include: {
-    //     model: Users,
-    //     model: Vehicles,
+    //     model: users,
+    //     model: vehicles,
     //   },
     // });
 
