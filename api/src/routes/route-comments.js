acdebea6d1964a798ef42e2dac22707fdb96dc7f
arielbg00 <ariel-bravo96@hotmail.com>
2022-11-16 18:02:29 -0300

    feat route comments

--- api/src/routes/route-comments.js
+++ api/src/routes/route-comments.js
@@ -1,38 +1,24 @@
 const { Router } = require("express");
 const router = Router();
-const { Comments } = require("../db.js");
-const {getAllComentarios } = require("../middlewares/comments");
+const { postComments, getAllComentarios } = require("../middlewares/comments");
 const comments = require("../middlewares/comments.json");
 
 router.post("/", async (req, res) => {
   try {
-    const { comment } = req.body;
-    if (!comment) {
-      return res.status(400).json({ error: "There is nothing to comment" });
-    }
-    await Comments.create(comment);
-    return res.status(200).json(comment);
+    const response = await postComments(req.body);
+    res.status(200).json(response);
   } catch (error) {
-
     res.status(400).json(error);
-
   }
 });
+
 router.get("/", async (req, res) => {
   try {
-
-    // const comentarios = await Comments.findAll({
-    //   include: {
-    //     model: users,
-    //     model: vehicles,
-    //   },
-    // });
-
+    const comments = await getAllComentarios();
     return res.status(200).json(comments);
   } catch (error) {
     res.status(400).json(error);
-
-
   }
 });
+
 module.exports = router;
