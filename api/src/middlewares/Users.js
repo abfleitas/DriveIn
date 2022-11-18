410879b89885b59a695b34aabb8dd66750b2ae25
Javymoyano <javymoyano80@gmail.com>
2022-11-18 18:24:50 -0300

    xx

--- api/src/middlewares/Users.js
+++ api/src/middlewares/Users.js
@@ -1,71 +1,64 @@
-const {Users, Vehicles, Rent} = require('../db.js')
+const { Users, Vehicles, Rent } = require("../db.js");
 
 async function postUser(req, res) {
-    const { name, lastName, phone, whatsapp, email, password} = req.body;
-    const newUser ={ name, lastName, phone, whatsapp, email, password };
-    try {
-      if (
-        !name ||
-        !lastName ||
-        !phone ||
-        !whatsapp ||
-        !email ||
-        !password 
-      ) {
-        return res.json({ error: "Incomplete information" });
-      }
-
-      const existUser = await Users.findOne({where: {email}});
-
-      if(existUser) return res.status(404).send("Ya existe un usuario con el ese e-mail")
-      
-      await Users.create(newUser);
-      res.status(200).json("Usuario creado");
-      return;
-    } catch (error) {
-      res.json(error);
-      return;
+  const { name, lastName, phone, whatsapp, email, password } = req.body;
+  const newUser = { name, lastName, phone, whatsapp, email, password };
+  try {
+    if (!name || !lastName || !phone || !whatsapp || !email || !password) {
+      return res.json({ error: "Incomplete information" });
     }
-  }
 
+    const existUser = await Users.findOne({ where: { email } });
+
+    if (existUser)
+      return res.status(404).send("Ya existe un usuario con el ese e-mail");
+
+    await Users.create(newUser);
+    res.status(200).json("Usuario creado");
+    return;
+  } catch (error) {
+    res.json(error);
+    return;
+  }
+}
 
 async function getUserById(req, res) {
-    try {
-      const {id} = req.params;
-      
-      let user = await Users.getByPk(id);
-      user? res.status(200).json(user) : res.status(400).json('No existe ese usuario');
-  
-    } catch (error) {
-      res.json(error);
-      return;
-    }
+  try {
+    const { id } = req.params;
+
+    let user = await Users.getByPk(id);
+    user
+      ? res.status(200).json(user)
+      : res.status(400).json("No existe ese usuario");
+  } catch (error) {
+    res.json(error);
+    return;
   }
+}
 
 async function getUsers(req, res) {
-    try {
-
-      let users = await Users.findAll({
-        include: {
-          model: Vehicles,
-          model: Rent
-        }
-      });
-      return res.status(200).json(users)
-    } catch (error) {
-      res.json(error);
-      return;
-    }
-  }
-
-async function getUserByEmail(req, res){
   try {
-    const {email} = req.params;
-    const exist = await Users.findOne({
-      where: {email},
+    let users = await Users.findAll({
       include: {
-        model: Vehicles
-      }
+        model: Vehicles,
+        model: Rent,
+      },
+    });
+    return res.status(200).json(users);
+  } catch (error) {
+    res.json(error);
+    return;
+  }
+}
+
+async function getUserByEmail(req, res) {
+  try {
+    const { email } = req.params;
+    const exist = await Users.findOne({
+      where: { email },
+      include: {
+        model: Vehicles,
+      },
     });
     return res.status(200).send(exist);
   } catch (error) {
@@ -73,20 +66,21 @@ async function getUserByEmail(req, res){
   }
 }
 
-async function getLoginUser(req, res){
+async function getLoginUser(req, res) {
   try {
     const { email, password } = req.body;
     const exist = await Users.findOne({
       where: {
         email,
-        password
+        password,
       },
       include: {
-        model: Vehicles
-      }
+        model: Vehicles,
+        model: Rent,
+      },
     });
-    if(exist) return res.status(200).send(exist)
-    return res.status(400).send("No existe el usuario con esos datos")
+    if (exist) return res.status(200).send(exist);
+    return res.status(400).send("No existe el usuario con esos datos");
   } catch (error) {
     res.status(404).send(error);
   }
@@ -97,5 +91,5 @@ module.exports = {
   getUserById,
   getUsers,
   getUserByEmail,
-  getLoginUser
-}
+  getLoginUser,
+};
