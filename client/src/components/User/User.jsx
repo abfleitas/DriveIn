6db85ed50fed6cb51d5deab756880cffd878c789
abfleitas <leonardo.fleitas.sk@gmail.com>
2022-11-18 08:51:06 -0300

    botones de usuario y cerrar sesion

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -4,6 +4,8 @@ import { exitSesion, refreshAuthUser } from "../../redux/actions/actions";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import icon from "../../images/flecha.png"
+import "./user.css"
 
 export const User = ({img, name}) => {
     const { isAuthenticated, logout, isLoading } = useAuth0();
@@ -16,7 +18,6 @@ export const User = ({img, name}) => {
      },[isLoading])
 
     const handlerExitSession = (e) => {
-        e.preventDefault();
         if (isAuthenticated) {
           localStorage.removeItem("UserLogin");
           logout();
@@ -28,21 +29,26 @@ export const User = ({img, name}) => {
     console.log(userLogin);
 
     return (
-        <nav className="w-[200px] h-[50px] bg-slate-900 rounded-xl">
+        <nav className="w-[200px] h-[50px] bg-slate-900 rounded-xl header">
             <div className="w-full flex flex-row justify-around">
                 <figure className="w-[40px] h-[40px] ml-4 mt-[4px] m-auto">
                     <img src={img} alt="img-user" className=" w-full h-full rounded-full"/>
                 </figure>
                 <span className="text-white m-auto">{name}</span>
             </div>
-            <button onClick={handlerExitSession}>Salir</button>
-            {/* <ul>
+            <ul className="z-999 -mt-[2px] ml-16 hidden absolute r-0 w-[130px] bg-slate-900 list">
                 <li>
-                    <button>
+                    <button className="w-full text-white border-none hover:bg-[#34d399]">Usuario</button>
+                </li>
+                {/* <li>
+                    <button className="w-full text-white border-none">Favoritos</button>
+                </li> */}
+                <li className="list__item">
+                    <button onClick={() => handlerExitSession()} className="w-full text-white border-none hover:bg-[#34d399]">
                         Salir
                     </button>
                 </li>
-            </ul> */}
+            </ul>
         </nav>
     )
 }
