b9cd13d0953a446431da7ad63fa8d6992dcded40
luqasmagra <luqasmagra@gmail.com>
2022-11-20 20:13:51 -0300

    Modal payment y bugs en consola

--- client/src/components/Favorites/Favorites.jsx
+++ client/src/components/Favorites/Favorites.jsx
@@ -9,41 +9,50 @@ export default function Favorites() {
   const favorites = useSelector((state) => state.favorites);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  useEffect(()=>{
-    dispatch(userFavorite())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(userFavorite());
+  }, [dispatch]);
 
   const irAtras = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     navigate(-1);
-  }
-console.log(navigate)
+  };
+
   return (
     <>
       <div className="bg-[#D9D9D9] min-h-screen">
         <div>
           <Navbar />
         </div>
-        <button onClick={irAtras} className="bg-[#2E3A46] text-white hover:bg-[#41D3C0] px-4 py-2 rounded-xl mt-4">Ir Atras</button>
+        <button
+          onClick={irAtras}
+          className="bg-[#2E3A46] text-white hover:bg-[#41D3C0] px-4 py-2 rounded-xl mt-4"
+        >
+          Ir Atras
+        </button>
         <div className="w-3/5 flex flex-wrap gap-x-16 gap-y-24 mt-10 justify-around m-auto">
           {/* css maquetadores, son la mera verg.... */}
-        {favorites.length ? favorites.map((v, index) => {
-          return <div>
-            <Card
-          key={index + v.id}
-          id={v.id}
-          brand={v.brand}
-          model={v.model}
-          score={v.score}
-          airconditioning={v.airconditioning}
-          transmition={v.transmition}
-          seats={v.seats}
-          category={v.category}
-          photo={v.photo}
-          price={v.initialPrice}
-        />
-          </div>
-        }) : null}
+          {favorites.length
+            ? favorites.map((v, index) => {
+                return (
+                  <div key={index}>
+                    <Card
+                      key={index + v.id}
+                      id={v.id}
+                      brand={v.brand}
+                      model={v.model}
+                      score={v.score}
+                      airconditioning={v.airconditioning}
+                      transmition={v.transmition}
+                      seats={v.seats}
+                      category={v.category}
+                      photo={v.photo}
+                      price={v.initialPrice}
+                    />
+                  </div>
+                );
+              })
+            : null}
         </div>
       </div>
     </>
--- client/src/components/Users/Users.jsx
+++ client/src/components/Users/Users.jsx
@@ -1,91 +1,99 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import { getCity, getCities } from '../../redux/actions/actions';
-import "./Users.css"
+import "./Users.css";
 
 const Users = () => {
+  const [UsersListLocal, setUsersListLocal] = useState([]);
 
-
-
-    const [UsersListLocal, setUsersListLocal] = useState([])
-
-
-    //-------------------------Users List-----------------------------------------
-    const fun = async () => {
-        let list = await axios.get("/user");
-        list = list.data.sort(function (a, b) {
-            if (a.id > b.id) {
-              return 1;
-            }
-            if (a.id < b.id) {
-              return -1;
-            }
-            return 0;
-          })
-        console.log(list);
-        const UsersList = list.map(user => (
-            <div className="Users_Unit" key={user.id}>
-                <img src={user.photo} alt="User" className="Users_photo"></img>
-                <p style={{width:"20px"}}>{user.id}</p>
-                <div>
-                {user.role === 1 && <p className="Users_Text4">Usuario</p>}
-                {user.role === 2 && <p className="Users_Text4">Moderador</p>}
-                {user.role === 3 && <p className="Users_Text4">Administrador</p>}
-                {user.role === 4 && <p className="Users_Text4">Dueño</p>}
-                </div>
-
-                    <p className="Users_Text3">{`${user.name} ${user.lastName}`}</p>
-
-
-                <p style={{width:"180px"}}>{user.email}</p>
-                <p style={{margin:"0px 0px 0px 50px"}}>{user.phone}</p>
-                {user.active ?
-                    <p style={{color: "green"}} className="Users_Text2">Activa</p>:
-                    <p style={{color: "red"}} className="Users_Text2">No activa</p>
-                    }
-                    {
-                        user.active ?
-                        <button className="Users_Button" onClick={statusChangeHandler} value={[user.active, user.id]}>Desabilitar</button>:
-                        <button className="Users_Button" onClick={statusChangeHandler} value={[user.active, user.id]}>Habilitar</button>
-                    }
-            </div>
-        ));
-        setUsersListLocal(UsersList);
-    }
-    //-------------------------Users List-----------------------------------------
-
-    useEffect(() => {
-        fun()
-    }, [])
-
-    const statusChangeHandler = async (event) => {
-        const state = event.target.value.split(",")
-        console.log(state);
-        await axios.put(`/user/${state[1]}`, {active : (state[0] === "true" ? false : true)})
-        fun()
-    };
-
-
-
-    return (
-        <div  className="Users_Main">
-            <div  className="Users_Second">
-                <p>Lista de Usuarios:</p>
-                <div className="Users_Sections_Titles">
-                    <p style={{margin:"0px 0px 0px 120px"}}>Id</p>
-                    <p style={{margin:"0px 0px 0px 50px"}}>Role</p>
-                    <p style={{margin:"0px 0px 0px 145px"}}>Name</p>
-                    <p style={{margin:"0px 0px 0px 160px"}}>Email</p>
-                    <p style={{margin:"0px 0px 0px 120px"}}>Telefono</p>
-                    <p style={{margin:"0px 0px 0px 20px"}}>Activo</p>
-                </div>
-                <div className="Users_Unit_Box">
-                    {UsersListLocal}
-                </div>
-            </div>
+  //-------------------------Users List-----------------------------------------
+  const fun = async () => {
+    let list = await axios.get("/user");
+    list = list.data.sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1;
+      }
+      if (a.id < b.id) {
+        return -1;
+      }
+      return 0;
+    });
+    console.log(list);
+    const UsersList = list.map((user) => (
+      <div className="Users_Unit" key={user.id}>
+        <img src={user.photo} alt="User" className="Users_photo"></img>
+        <p style={{ width: "20px" }}>{user.id}</p>
+        <div>
+          {user.role === 1 && <p className="Users_Text4">Usuario</p>}
+          {user.role === 2 && <p className="Users_Text4">Moderador</p>}
+          {user.role === 3 && <p className="Users_Text4">Administrador</p>}
+          {user.role === 4 && <p className="Users_Text4">Dueño</p>}
         </div>
-    )
-}
+
+        <p className="Users_Text3">{`${user.name} ${user.lastName}`}</p>
+
+        <p style={{ width: "180px" }}>{user.email}</p>
+        <p style={{ margin: "0px 0px 0px 50px" }}>{user.phone}</p>
+        {user.active ? (
+          <p style={{ color: "green" }} className="Users_Text2">
+            Activa
+          </p>
+        ) : (
+          <p style={{ color: "red" }} className="Users_Text2">
+            No activa
+          </p>
+        )}
+        {user.active ? (
+          <button
+            className="Users_Button"
+            onClick={statusChangeHandler}
+            value={[user.active, user.id]}
+          >
+            Desabilitar
+          </button>
+        ) : (
+          <button
+            className="Users_Button"
+            onClick={statusChangeHandler}
+            value={[user.active, user.id]}
+          >
+            Habilitar
+          </button>
+        )}
+      </div>
+    ));
+    setUsersListLocal(UsersList);
+  };
+  //-------------------------Users List-----------------------------------------
+
+  useEffect(() => {
+    fun();
+  }, []);
+
+  const statusChangeHandler = async (event) => {
+    const state = event.target.value.split(",");
+    console.log(state);
+    await axios.put(`/user/${state[1]}`, {
+      active: state[0] === "true" ? false : true,
+    });
+    fun();
+  };
+
+  return (
+    <div className="Users_Main">
+      <div className="Users_Second">
+        <p>Lista de Usuarios:</p>
+        <div className="Users_Sections_Titles">
+          <p style={{ margin: "0px 0px 0px 120px" }}>Id</p>
+          <p style={{ margin: "0px 0px 0px 50px" }}>Role</p>
+          <p style={{ margin: "0px 0px 0px 145px" }}>Name</p>
+          <p style={{ margin: "0px 0px 0px 160px" }}>Email</p>
+          <p style={{ margin: "0px 0px 0px 120px" }}>Telefono</p>
+          <p style={{ margin: "0px 0px 0px 20px" }}>Activo</p>
+        </div>
+        <div className="Users_Unit_Box">{UsersListLocal}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Users;
