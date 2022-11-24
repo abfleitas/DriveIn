c1f8083fd3c643c76f59768c0eae12b92ba464a4
unknown <verojuy.gonzalez@gmail.com>
2022-11-24 14:51:26 -0300

    preparando comentarios

--- client/src/components/CommentsForm/CommentsForm.jsx
+++ client/src/components/CommentsForm/CommentsForm.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { postComment } from "../../redux/actions/actions";
+import StarRating from "../Star/Star_rating";
 
 export default function CommentsForm() {
 
    const dispatch = useDispatch();
 
    const [input, setInput] = useState({
+      confort: "",
+      performance: "",
+      security: "",
+      priceQuality: "",
+      recommended: "",
       description: "",
       id: []
    });
@@ -14,7 +20,9 @@ export default function CommentsForm() {
    const handleChange = (e) => {
       setInput({
          ...input,
-         description: e.target.value
+         [e.target.name]: e.target.value,
+
+
       });
    };
 
@@ -30,9 +38,21 @@ export default function CommentsForm() {
    return (
       <div>
          <form onSubmit={handleSubmit}>
+            <label>Comodidad</label>
+            <StarRating />
+
+            <label>Desempeño</label>
+            <StarRating />
+            <label>Seguridad</label>
+            <StarRating />
+            <label>Relacion precio calidad</label>
+            <StarRating />
+            <label>Recomendado</label>
+            <StarRating />
             <label>Deja tu comentario</label>
-            <br/>
+            <br />
             <textarea
+               name="description"
                rows="5"
                cols="40"
                value={input.description}
@@ -40,7 +60,8 @@ export default function CommentsForm() {
                className="border-[#009A88] border-2 rounded-lg"
             >
             </textarea>
-            <br/>
+
+            <br />
             <button className="py-0 px-3 bg-[#009A88] rounded-lg text-white">Enviar</button>
          </form>
       </div>
