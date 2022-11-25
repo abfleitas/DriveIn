d2c0d7ca62d4848b3970b10e924c6e0c01ae09b6
arielbg00 <ariel-bravo96@hotmail.com>
2022-11-25 17:46:29 -0300

    mas filtros y cambios y referencia de ciudada

--- client/src/components/Administrador/UpdateVehicle.jsx
+++ client/src/components/Administrador/UpdateVehicle.jsx
@@ -1,8 +1,44 @@
 import { Edit, SimpleForm, TextInput, NumberInput, BooleanInput, ImageInput } from "react-admin";
 
+const validateCreation = (values) => {
+   const errors = {};
+   if (!values.brand) errors.brand = "Escriba la marca";
+   else if (!/^[a-zA-Z0-9ñ]+$/.test(values.brand)) {
+      errors.brand = "Solo se aceptan letras y numeros";
+   }
+   if (!values.model) errors.model = "Escriba el modelo";
+   else if (!/^[a-zA-Z0-9ñ]+$/.test(values.model)) {
+      errors.model = "Solo se aceptan letras y numeros";
+   }
+   if (!values.year) errors.year = "Escriba el año";
+   else if (values.year > 2023 || values.year < 2010) {
+      errors.year = "Solo se permite el año entre 2010 y 2023";
+   }
+   if (!values.color) errors.color = "Escriba el color";
+   else if (!/^[a-zA-Zñ]+$/.test(values.color)) {
+      errors.color = "Solo se aceptan letras";
+   }
+   if (!values.transmition) errors.transmition = "Seleccione la transmisión";
+   if (values.seats < 2) {
+      errors.seats = "Selecione 2 o mas asientos";
+   }
+   if (!values.category) errors.category = "Escriba la categoría";
+   else if (!/^[a-zA-Zñ]+$/.test(values.category)) {
+      errors.category = "Solo se aceptan letras";
+   }
+   if (values.initialPrice < 1) {
+      errors.initialPrice = "Precio tiene que ser mayor a 1";
+   }
+   if (!values.cityId) errors.cityId = "Escriba la ciudad";
+   // else if (!/^[a-zA-Zñ]+$/.test(values.cityId)) {
+   //    errors.cityId = "Solo se aceptan letras";
+   // }
+   return errors;
+};
+
 export const UpdateVehicle = () => (
    <Edit title="Edit a vehicle">
-      <SimpleForm>
+      <SimpleForm validate={validateCreation}>
          <NumberInput disabled label="Id" source="id" />
          <TextInput source="brand" />
          <TextInput source="model" />
