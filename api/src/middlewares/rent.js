8c63f8fcc28ede416f463014b96ab983ba317249
Agustin <jagustin.rm@gmail.com>
2022-11-28 11:57:31 -0300

    arreglo de rent

--- api/src/middlewares/rent.js
+++ api/src/middlewares/rent.js
@@ -11,6 +11,7 @@ const {dashboard} = require ("../middlewares/dashboard")
 const allRents = async (req, res) => {
   try {
     const {order, corte, pagina} = dashboard(req.query)
+    
     if(!(await Rent.findAll()).length) await Rent.bulkCreate(rents);
 
     if(req.query.id) {
@@ -33,11 +34,30 @@ const allRents = async (req, res) => {
        return res.status(200).send(rent)
     }
     
-let cantidad = await rentsInactive.length
-        return res.header("Content-Range",`0-10/${cantidad}`).status(200).send(rentsInactive)
-      };
-    }
-
+    let filter = JSON.parse(req.query.filter);
+    let categoria = filter.category
+    console.log(categoria);
+    if (categoria) {
+      const users = await  Users.findAll()
+      let rentsInactive = await Rent.findAll({
+        include : {
+          model: Vehicles,
+          required: false
+        },
+        order: order,
+        limit:corte,
+        offset: pagina,
+        where: {active: false}
+      })
+      await rentsInactive.forEach(rent => {
+        const user = users.filter(user => user.id === rent.userId);
+        rent.dataValues.userEmail = user[0].dataValues.email
+        rent.dataValues.userName = user[0].dataValues.name
+        rent.dataValues.vehicle = rent.dataValues.vehicle.brand + " " + rent.dataValues.vehicle.model
+      })
+      let cantidad = await rentsInactive.length
+      return res.header("Content-Range",`0-10/${cantidad}`).status(200).send(rentsInactive)
+    };
     if (order || corte || pagina) {
       const users = await  Users.findAll()
       const response = await  Rent.findAll({
