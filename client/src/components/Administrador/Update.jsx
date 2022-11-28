18dcfe1e122c32049ca931975f81a25d2e6010d7
Frandel Rodriguez <frandel321@hotmail.com>
2022-11-28 17:12:36 -0600

    cambios necesario

--- client/src/components/Administrador/Update.jsx
+++ client/src/components/Administrador/Update.jsx
@@ -16,25 +16,6 @@ const imageCloudChangeHandler = (event) => {
 
 const validateCreation = (values) => {
    const errors = {};
-   if(values.photo) {values.photo = ImageCloud}
-   if (!values.name) errors.name = "Escriba el nombre";
-   else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(values.name)) {
-      errors.name = "Solo se aceptan letras";
-   }
-   if (!values.lastName) errors.lastName = "Escriba el apellido";
-   else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(values.lastName)) {
-      errors.lastName = "Solo se aceptan letras";
-   }
-   if (!values.email) errors.email = "Escriba un email";
-   else if (!/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(values.email)) {
-      errors.email = "Email inválido";
-   }
-   if (!values.password) errors.password = "Escriba un password";
-   else if (!/^.{4,12}$/.test(values.password)) {
-      errors.password = "Password inválido";
-   }
-   if (!values.role) errors.role = "Elija un rol";
-
    return errors;
 };
 
